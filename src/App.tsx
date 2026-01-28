e6845a847959a23e6a4a0d12035585e5cf90532d
Unoguna <kimjungho.kr@gmail.com>
2026-01-28 13:12:32 +0900

    [init] 프로젝트 기초 세팅

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500 text-white p-4 text-2xl">Tailwind OK?</div>
+      <div className="bg-blue-500 p-10 rounded-xl">
+        적용되면 파랗고 둥글어야 함
+      </div>
+    </>
+  );
+}
+
+export default App;
